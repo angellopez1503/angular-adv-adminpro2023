a598ddf854ea85bd16ea54afa39f47a4aa590cd4
Angel Lopez <arturolopez15@hotmail.com>
2023-10-05 23:35:47 -0500

    Seccion 7 - Cambiar el CSS principal de forma dinámica

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,6 +8,7 @@ import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { ComponentsModule } from '../components/components.module';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ComponentsModule } from '../components/components.module';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ],
   imports: [
     CommonModule,
@@ -23,13 +25,13 @@ import { ComponentsModule } from '../components/components.module';
     FormsModule,
     ComponentsModule,
 
-
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent
   ],
 })
 export class PagesModule {}
