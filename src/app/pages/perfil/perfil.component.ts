ef1ef33edd9f815ca09f1d98bc91f5a8ea92c8e8
Angel Lopez <arturolopez15@hotmail.com>
2023-10-12 00:18:00 -0500

    Fin seccion 15

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario.service';
 import { Usuario } from 'src/app/models/usuario.model';
+import { FileUploadService } from '../../services/file-upload.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-perfil',
@@ -9,40 +11,73 @@ import { Usuario } from 'src/app/models/usuario.model';
   styleUrls: ['./perfil.component.css'],
 })
 export class PerfilComponent implements OnInit {
-
-  public perfilForm!:FormGroup 
-  public usuario!:Usuario|any
+  public perfilForm!: FormGroup;
+  public usuario!: Usuario | any;
+  public imagenSubir!: File;
+  public imgTemp: any = '';
 
   constructor(
-    private formBuilder:FormBuilder,
-    private usuarioService:UsuarioService,
+    private formBuilder: FormBuilder,
+    private usuarioService: UsuarioService,
+    private fileUploadService: FileUploadService
   ) {
-
-    this.usuario = usuarioService?.usuario
-
+    this.usuario = usuarioService?.usuario;
   }
 
   ngOnInit(): void {
-
     this.perfilForm = this.formBuilder.group({
-      name:[this.usuario.name,Validators.required],
-      email:[this.usuario.email,[Validators.required, Validators.email]]
-    })
-
+      name: [this.usuario.name, Validators.required],
+      email: [this.usuario.email, [Validators.required, Validators.email]],
+    });
   }
 
-  actualizarPerfil(){
+  actualizarPerfil() {
     console.log(this.perfilForm.value);
     this.usuarioService.actualizarPerfil(this.perfilForm.value).subscribe(
-      (res:any) => {
-        console.log(res);
-        const {name,email} = res.usuario
-        this.usuario.name = name
-        this.usuario.email = email
-      },  
-      err=>{
+      (res: any) => {
+        const { name, email } = res.usuario;
+        this.usuario.name = name;
+        this.usuario.email = email;
+        Swal.fire('Guardado', 'Cambios fueron guardados', 'success');
+      },
+      (err) => {
         console.log(err);
+        Swal.fire('Error', err.error.msg, 'error');
       }
-    )
+    );
+  }
+
+  cambiarImagen(event: any) {
+    const file = event.target.files[0];
+    this.imagenSubir = file;
+
+    if (!file) {
+      this.imgTemp = null;
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      this.imgTemp = reader.result;
+      console.log(reader.result);
+    };
+  }
+
+  subirImagen() {
+    this.fileUploadService
+      .actualizarFoto(this.imagenSubir, 'usuarios', this.usuario.uid)
+      .then((data) => {
+        if (data.ok) {
+          this.usuario.img = data.nombreArchivo;
+          Swal.fire('Guardado', 'Imagen de usuario actualizada', 'success');
+        } else {
+          Swal.fire('Error', data.msg, 'error');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const base_url = environment.base_url
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  constructor() { }
+  
+
+  async actualizarFoto(
+    archivo:File,
+    tipo:'usuarios'|'medicos'|'hospitales',
+    id:string
+  ){
+    
+    try {
+
+      const url=`${base_url}/upload/${tipo}/${id}`
+      const formData = new FormData()
+      formData.append('imagen',archivo)
+
+      const resp = await fetch(url,{
+        method:'put',
+        headers:{
+          'x-token':localStorage.getItem('token')||''
+        },
+        body:formData
+      })
+
+      const data = await resp.json()
+      return data
+      // console.log("dddd"+data);
+      // if(data.ok){
+      //   return data.nombreArchivo
+      // }else{
+      //   console.log(data.msg);
+      //   return null
+      // }
+
+      // console.log(data);
+      // return 'nombre imagen'
+      
+    } catch (error) {
+      console.log(error);
+      return null
+    }
+
+    
+
+  }
+
+}
